50c0f5153c5eac963994177b4ab3efbbae55573e
Layla Heinemann <404ghost@gmail.com>
2024-01-16 21:04:29 +0100

    changed look of plot

--- EJpowerlaw_plot.m
+++ EJpowerlaw_plot.m
@@ -7,6 +7,8 @@ y4=x.^50;
 y5=x.^Inf;
 
 clf
-plot(x,y1,x,y2,x,y3,x,y4,x,y5)
+plot(x,y1,x,y2,x,y3,x,y4,x,y5,'LineWidth',2)
 ylim([0 2])
-legend('n=1','n=5','n=15','n=50','Bean')
+legend('n=1','n=5','n=15','n=50','Bean','Location','northwest')
+xlabel('J/J_{C}')
+ylabel('E/E_{0}')
